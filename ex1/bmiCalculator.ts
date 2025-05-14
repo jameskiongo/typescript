daf4a19fd062024f55ad44f8abd6aa60354109f8
jameskiongo <kiongojames2000@gmail.com>
2025-05-14 22:24:53 +0300

    practising basic typescript

---
+++ ex1/bmiCalculator.ts
@@ -0,0 +1,17 @@
+const calculateBmi = (height: number, weight: number) => {
+  const convert = Math.pow(height / 100, 2);
+  const bmi = weight / convert;
+  console.log(bmi);
+  if (bmi < 18.5) {
+    return "underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Normal Range";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return "Overweight";
+  } else if (bmi > 30) {
+    return "obese";
+  } else {
+    throw new Error("Invalid");
+  }
+};
+console.log(calculateBmi(180, 74));
---
+++ ex1/exerciseCalculator.ts
@@ -0,0 +1,13 @@
+interface Days {
+  monday: number;
+  tuesday: number;
+  wednesday: number;
+  thursday: number;
+  friday: number;
+  saturday: number;
+  sunday: number;
+}
+const calculateExercises = (exercised: number[], target: number) => {
+  console.log(exercised);
+  console.log(target);
+};
