8ffb427f0ab3f67b2cfa22ae42188b6d5e59b26e
jameskiongo <kiongojames2000@gmail.com>
2025-05-27 11:09:26 +0300

    fix: Adding types

--- diaries/frontend/src/components/Content.tsx
+++ diaries/frontend/src/components/Content.tsx
@@ -1,21 +1,15 @@
 import React from "react";
-interface CoursePart {
-  name: string;
-  exerciseCount: number;
-}
+import type { CoursePart } from "../types";
 function Content({ courseParts }: { courseParts: CoursePart[] }) {
   return (
-    <div>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-    </div>
+    <>
+      {courseParts.forEach((part) => {
+        if (part.kind === "background") {
+          console.log("see the following:", part.backgroundMaterial);
+        }
+      })}
+    </>
+    // <div></div>
   );
 }
 
